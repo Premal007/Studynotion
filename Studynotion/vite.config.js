2de77f6886ac920e7f4f986691c92e03f7883629
Premal007 <premalpatel.abc@gmail.com>
2025-06-25 10:44:08 +0530

    vite.config

--- Studynotion/vite.config.js
+++ Studynotion/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
+    base: '/', // ✅ This is the important part
     server: {
       port: Number(env.VITE_PORT) || 3000,
     },
